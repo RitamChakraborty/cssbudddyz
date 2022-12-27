0f9f0f1b327414e2375f6cf2ec8fba14b444524b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-12-27 17:58:18 +0530

    Add project blink

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import {Route, Router, Routes} from '@solidjs/router';
 import {render} from 'solid-js/web';
 import Home from "./pages/home/Home";
 import Bee from './projects/bee/Bee';
+import Blink from './projects/blink/Blink';
 import Red from "./projects/red/Red";
 
 render(() => (
@@ -10,6 +11,7 @@ render(() => (
                 <Route path="/" component={Home}/>
                 <Route path="/red" component={Red}/>
                 <Route path="/bee" component={Bee}/>
+                <Route path="/blink" component={Blink}/>
             </Routes>
         </Router>
     ),
---
+++ src/projects/blink/Blink.tsx
@@ -0,0 +1,36 @@
+import './Blink.scss';
+
+export default function Blink() {
+    return (
+        <div id="Blink">
+            <div class="eyes">
+                <div class="eye-container">
+                    <div class="lid-container">
+                        <div class="lid lid1"></div>
+                    </div>
+                    <div class="wrapper">
+                        <div class="eye">
+                            <div class="iris">
+                                <div class="retina"></div>
+                            </div>
+                        </div>
+                        <div class="lid lid2"></div>
+                    </div>
+                </div>
+                <div class="eye-container">
+                    <div class="lid-container">
+                        <div class="lid lid1"></div>
+                    </div>
+                    <div class="wrapper">
+                        <div class="eye">
+                            <div class="iris">
+                                <div class="retina"></div>
+                            </div>
+                        </div>
+                        <div class="lid lid2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}

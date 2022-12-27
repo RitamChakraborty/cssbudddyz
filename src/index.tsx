a95a90bb2890ebc74f9c4e3e5c14b49333eca722
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-12-27 17:45:11 +0530

    Add project bee

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import {Route, Router, Routes} from '@solidjs/router';
 import {render} from 'solid-js/web';
 import Home from "./pages/home/Home";
+import Bee from './projects/bee/Bee';
 import Red from "./projects/red/Red";
 
 render(() => (
@@ -8,6 +9,7 @@ render(() => (
             <Routes>
                 <Route path="/" component={Home}/>
                 <Route path="/red" component={Red}/>
+                <Route path="/bee" component={Bee}/>
             </Routes>
         </Router>
     ),
---
+++ src/projects/bee/Bee.tsx
@@ -0,0 +1,28 @@
+import './Bee.scss';
+
+export default function Bee() {
+    return (
+        <div id="Bee">
+            <div class="bee">
+                <div class="wing wing-1"></div>
+                <div class="wing wing-2"></div>
+                <div class="sting"></div>
+                <div class="antenna-wrapper antenna-wrapper-1">
+                    <div class="antenna"></div>
+                    <div class="tip"></div>
+                </div>
+                <div class="body">
+                    <div class="stripe strip-1"></div>
+                    <div class="stripe strip-2"></div>
+                    <div class="stripe strip-3"></div>
+                </div>
+                <div class="antenna-wrapper antenna-wrapper-2">
+                    <div class="antenna"></div>
+                    <div class="tip"></div>
+                </div>
+                <div class="smile"></div>
+                <div class="eye"></div>
+            </div>
+        </div>
+    )
+}

d03b66c8929b052a7d22a21d4e5d999134dcc529
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2022-12-28 17:21:04 +0530

    Make project bee responsive

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,23 +5,28 @@ import './Home.scss';
 export default function Home() {
     return (
         <div id="Home">
-            <Nav/>
-            A place to showcase all of ours css projects
-            <A href="/">Home</A>
-            <A href="/red">Red</A>
-            <A href="/bee">Bee</A>
-            <A href="/blink">Blink</A>
-            <A href="/eye">Eye</A>
-            <A href="/brush-cursor">Brush Cursor</A>
-            <A href="/eight">Eight</A>
-            <A href="/slightly-smiling">Slightly Smiling</A>
-            <A href="/floating-navigation">Floating Navigation</A>
-            <A href="/switch">Switch</A>
-            <A href="/pacman">Pacman</A>
-            <A href="/rolex">Rolex</A>
-            <A href="/smile-injection">Smile Injection</A>
-            <A href="/santa">Santa</A>
-            <A href="/butterfly"></A>
+                <Nav/>
+                A place to showcase all of ours css projects
+                <ul>
+                        <li><A href="/">Home</A></li>
+                        <li><A href="/red">Red</A></li>
+                        <li><A href="/bee">Bee</A></li>
+                        <li><A href="/blink">Blink</A></li>
+                        <li><A href="/eye">Eye</A></li>
+                        <li><A href="/brush-cursor">Brush Cursor</A></li>
+                        <li><A href="/eight">Eight</A></li>
+                        <li><A href="/slightly-smiling">Slightly Smiling</A></li>
+                        <li><A href="/floating-navigation">Floating Navigation</A></li>
+                        <li><A href="/switch">Switch</A></li>
+                        <li><A href="/pacman">Pacman</A></li>
+                        <li><A href="/rolex">Rolex</A></li>
+                        <li><A href="/smile-injection">Smile Injection</A></li>
+                        <li><A href="/santa">Santa</A></li>
+                        <li><A href="/butterfly">Butterfly</A></li>
+                        <li><A href="/ripple">Ripple</A></li>
+                        <li><A href="/clock">Clock</A></li>
+                        <li><A href="/rocket">Rocket</A></li>
+                </ul>
         </div>
     );
 }

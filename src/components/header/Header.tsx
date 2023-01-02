ae6476e07d7d0acba411a41779a05b9f484b010b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-01-02 22:37:22 +0530

    Create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+export default function Header() {
+    return (
+        <div id="Header">
+            <header>
+                CSS Budddyz
+            </header>
+        </div>
+    )
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import './Home.scss';
 import {createSignal, For} from "solid-js";
+import Header from "../../components/header/Header";
 
 export default function Home() {
     const projectsUrls: readonly Project[] = [
@@ -55,6 +56,7 @@ export default function Home() {
 
     return (
         <div id="Home">
+            <Header/>
             <div class="container">
                 <For<Project> each={projectsUrls}>{(project, index) =>
                     <div class="card" style={createStyleForCard(index())}>

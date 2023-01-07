08bcc57ad5b3f9bf09662614fcada9446c98568b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-01-07 20:09:06 +0530

    Create theme switch animation

--- src/components/theme-switcher/ThemeSwitcher.tsx
+++ src/components/theme-switcher/ThemeSwitcher.tsx
@@ -1,14 +1,27 @@
 import "./ThemeSwitcher.scss";
+import {createSignal} from "solid-js";
 
 export default function ThemeSwitcher() {
+    const [angle, setAngle] = createSignal<number>(0);
+
+    function changeTheme() {
+        setAngle((value) => (value + 0.18));
+        console.log(angle());
+    }
+
     return (
-      <div id="ThemeSwitcher">
-          <div class="icon light">
-              <i class="fa-solid fa-sun"></i>
-          </div>
-          <div class="icon dark">
-              <i class="fa-solid fa-moon"></i>
-          </div>
-      </div>
+        <div
+            id="ThemeSwitcher"
+            style={{
+                transform: `rotate(${angle() * 1000}deg)`
+            }}
+        >
+            <div class="theme light" onclick={changeTheme}>
+                <i class="fa-regular fa-sun"></i>
+            </div>
+            <div class="theme dark" onclick={changeTheme}>
+                <i class="fa-regular fa-moon"></i>
+            </div>
+        </div>
     );
 }

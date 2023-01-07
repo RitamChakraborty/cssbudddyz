ecf3f1a1a6ebe504c0354685b86fb1b8bcb2191e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-01-08 01:24:30 +0530

    Update base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: './',
+  base: 'https://ritamchakraborty.github.io/cssbudddyz/',
   server: {
     port: 3000,
   },

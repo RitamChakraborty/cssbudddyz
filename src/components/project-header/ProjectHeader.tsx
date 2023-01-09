45379a208117542599f197f98dac2ac9202480be
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-01-09 10:11:16 +0530

    Update ProjectHeader ui

--- src/components/project-header/ProjectHeader.tsx
+++ src/components/project-header/ProjectHeader.tsx
@@ -1,7 +1,21 @@
+import IconButton from "../icon-button/IconButton";
+import {useNavigate} from "@solidjs/router";
+import "./ProjectHeader.scss";
+
 export default function ProjectHeader() {
+    const navigate = useNavigate();
+
+    function navigateBack() {
+        navigate('/')
+    }
+
     return (
         <div id="ProjectHeader">
-
+            <IconButton icon="fa-solid fa-arrow-left" onPressed={navigateBack}/>
+            <header>CSS Budddyz</header>
+            <div class="pull-down">
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
         </div>
     );
 }
--- src/projects/eight/Eight.tsx
+++ src/projects/eight/Eight.tsx
@@ -1,15 +1,19 @@
 import "./Eight.scss";
+import ProjectHeader from "../../components/project-header/ProjectHeader";
 
 export default function Eight() {
     return (
-        <div id="Eight">
-            <div class="container">
-                <div class="half-circle c1"></div>
-                <div class="half-circle b b1"></div>
-                <div class="half-circle b b2"></div>
-                <div class="half-circle c2"></div>
+        <>
+            <ProjectHeader/>
+            <div id="Eight">
+                <div class="container">
+                    <div class="half-circle c1"></div>
+                    <div class="half-circle b b1"></div>
+                    <div class="half-circle b b2"></div>
+                    <div class="half-circle c2"></div>
+                </div>
+                <p>HOVER</p>
             </div>
-            <p>HOVER</p>
-        </div>
+        </>
     )
 }
